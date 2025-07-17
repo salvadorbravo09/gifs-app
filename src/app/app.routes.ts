89ab7daebcf1dcb5c160acac9b2fbc819b50a80c
Salvador Bravo <sa.bravo0901@gmail.com>
2025-07-17 17:06:58 -0400

    refactor: restructure dashboard routes to include children for trending and search pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,20 +2,31 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
+    // Ruta principal de la aplicación
     path: 'dashboard',
     loadComponent: () =>
       import('./gifs/pages/dashboard-page/dashboard-page.component'),
+    // Rutas hijas de la página del dashboard
+    children: [
+      {
+        path: 'trending',
+        loadComponent: () =>
+          import('./gifs/pages/trending-page/trending-page.component'),
+      },
+      {
+        path: 'search',
+        loadComponent: () =>
+          import('./gifs/pages/search-page/search-page.component'),
+      },
+      // Ruta por defecto si no se especifica ninguna ruta hija
+      {
+        path: '**',
+        redirectTo: 'trending',
+      },
+    ],
   },
-  {
-    path: 'trending',
-    loadComponent: () =>
-      import('./gifs/pages/trending-page/trending-page.component'),
-  },
-  {
-    path: 'search',
-    loadComponent: () =>
-      import('./gifs/pages/search-page/search-page.component'),
-  },
+
+  // Ruta por defecto si no se especifica ninguna ruta
   {
     path: '**',
     redirectTo: 'dashboard',
