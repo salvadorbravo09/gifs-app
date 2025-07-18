b2997adf06eaf82ff66a620f01b69376db79c746
Salvador Bravo <sa.bravo0901@gmail.com>
2025-07-18 17:49:35 -0400

    feat: add Giphy API key and URL to environment configuration

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -3,4 +3,8 @@ export const environment = {
   companyName: 'Gifs',
   companyName2: 'App',
   companySlogan: 'Maneja tus gifs favoritos',
+
+  // API URL for fetching GIFs
+  giphyApiKey: 'kQ1Vo11VapL1hq1maOxuTFnwvd5pPkHr',
+  giphyUrl: 'https://api.giphy.com/v1',
 };
