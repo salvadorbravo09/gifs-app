48ab4243f4be533f1ef27870e14d0e4ade0d0240
Salvador Bravo <sa.bravo0901@gmail.com>
2025-07-17 22:20:32 -0400

    feat: integrate environment configuration into side menu header component

--- src/app/gifs/components/side-menu-header/side-menu-header.component.ts
+++ src/app/gifs/components/side-menu-header/side-menu-header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { environment } from '@environments/environment';
 
 @Component({
   selector: 'gifs-side-menu-header',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   templateUrl: './side-menu-header.component.html',
 })
 export class SideMenuHeaderComponent {
-
+  envs = environment;
 }
